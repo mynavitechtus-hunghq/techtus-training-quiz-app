1431961a0e9d2da9e1bf49b3ecf9a8119cfd356d
Hunine <hqhung201@gmail.com>
2026-01-02 14:05:33 +0700

    Define User and Role model

--- quiz-app-be/app/db/base_class.py
+++ quiz-app-be/app/db/base_class.py
@@ -1,9 +1,18 @@
+from sqlalchemy.sql.functions import func
+from sqlalchemy.sql.sqltypes import DateTime
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import Integer
 from typing import Any
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
+
 @as_declarative()
 class Base:
-    id: Any
+    id: Integer = Column(Integer, primary_key=True)
+    created_at: Column(DateTime, default=func.now())
+    updated_at: Column(DateTime, default=func.now(), onupdate=func.now())
+    deleted_at: Column(DateTime, nullable=True)
+
     __name__: str
 
     # Generate __tablename__ automatically
