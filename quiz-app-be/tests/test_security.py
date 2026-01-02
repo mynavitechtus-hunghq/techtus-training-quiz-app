2fa773355dfbe74827f3119657e2e392ea5c0b87
Hunine <hqhung201@gmail.com>
2026-01-02 17:02:16 +0700

    Implement JWT token creation and decoding logic

--- quiz-app-be/tests/test_security.py
+++ quiz-app-be/tests/test_security.py
@@ -1,4 +1,10 @@
-from app.core.security import get_password_hash, verify_password
+from app.core.security import (
+    get_password_hash,
+    verify_password,
+    create_access_token,
+)
+from jose import jwt
+from app.core.config import settings
 
 
 def test_password_hashing():
@@ -7,3 +13,10 @@ def test_password_hashing():
     assert hashed_password != password
     assert verify_password(password, hashed_password) is True
     assert verify_password("wrong_password", hashed_password) is False
+
+
+def test_create_access_token():
+    subject = "test_user"
+    token = create_access_token(subject)
+    payload = jwt.decode(token, settings.SECRET_KEY, [settings.ALGORITHM])
+    assert payload["sub"] == subject
