7534813c5dd1338305fa2dfef28ab076b4addfec
Hunine <hqhung201@gmail.com>
2026-01-02 15:00:57 +0700

    Setup alembic

--- quiz-app-be/app/models/user.py
+++ quiz-app-be/app/models/user.py
@@ -1,13 +1,15 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey, String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db.base_class import Base
 
 
 class User(Base):
-    email = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True)
-    role_id = Column(Integer, ForeignKey("role.id"), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String, unique=True, index=True, nullable=False
+    )
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    role_id: Mapped[int] = mapped_column(ForeignKey("role.id"), nullable=False)
 
-    role = relationship("Role", back_populates="users")
+    role: Mapped["Role"] = relationship("Role", back_populates="users")
